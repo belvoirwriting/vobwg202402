eeb0885c4b908a00e43a17fe39be8c43b3c530ea
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2024-02-18 19:18:14 +0000

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
 	siteURL: process.env.URL || 'http://localhost:8080',
-	siteName: "Shane Robinson's 11ty | TailwindCSS | Alpine.js Template",
+	siteName: "Vale of Belvoir Writing Group",
 	siteDescription:
-		'A highly configurable 11ty template to quickly spin up a fully functional website with TailwindCSS and Alpine.js baked in.',
+		'Helping writers find their voice since 2018',
 	siteImage: '/assets/images/site/default.png',
 	lang: 'en',
 	locale: 'en_us',
