e8fcf184d6fead81870f277628977a8989657148
belvoirwriting <69843158+belvoirwriting@users.noreply.github.com>
2024-02-20 15:00:12 +0000

    meta

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,18 +1,23 @@
 module.exports = {
 	siteURL: process.env.URL || 'http://localhost:8080',
-	siteName: "Vale of Belvoir Writing Group",
+	siteName: "Shane Robinson's 11ty | TailwindCSS | Alpine.js Template",
 	siteDescription:
-'A group for writers from the Vale
-of Belvoir and beyond',
+		'A highly configurable 11ty template to quickly spin up a fully functional website with TailwindCSS and Alpine.js baked in.',
 	siteImage: '/assets/images/site/default.png',
 	lang: 'en',
-	locale: 'en_uk',
-	authorName: 'Tris',
+	locale: 'en_us',
+	authorName: 'Shane Robinson',
+	authorURL: 'shane-robinson',
+	authorEmail: 'shane@shanerobinson.com',
+	twitterSite: '@shanerobinson',
+	twitterCreator: '@shanerobinson',
 	address: {
-		street: 'Parish Room, The Old School, Grantham Road',
-		city: 'Bottesford',
-		state: 'Nottingham, Leicestershire',
-		zip: 'NG13 0DF',
-		email: 'words@belvoirwriting.co.uk',
+		street: '123 Main Street',
+		city: 'Anytown',
+		state: 'KZ',
+		zip: '54321',
+		phoneDisplay: '+1 (123) 456-7890',
+		phoneCall: '+11234567890',
+		email: 'hello@domain.com',
 	},
 }
